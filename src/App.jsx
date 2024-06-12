d1cefe813d43f249a033669189d315a2a18e2b22
kauser1987 <kauser1987@gmail.com>
2024-06-12 16:27:42 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -37,10 +37,10 @@ const App = () => {
   return (
     <div className='w-full h-screen flex flex-col justify-center items-center bg-gray-900'>
         <h1 className='text-white font-semibold text-[3vw]'>Todo List</h1>
-      <div className='w-[30vw] h-[35vw] flex flex-col  items-center gap-4 py-4   rounded-lg bg-white'>
+      <div className='min-w-[30vw] h-[35vw] overflow-scroll flex flex-col  items-center gap-4 py-4   rounded-lg bg-white'>
         <div className='flex flex-col gap-4  items-center  font-semibold'>
         <div className=' flex border-2 rounded-full text-white gap-4'>
-          <input ref={inputref} className='h-[2vw] w-[15vw] flex items-center justify-center border-0 text-black outline-none px-2 rounded-lg' type="text" placeholder='enter your list ' />
+          <input ref={inputref} className='h-[4vw] w-[15vw] flex items-center justify-center border-0 text-black outline-none px-2 rounded-lg' type="text" placeholder='enter your list ' />
           <button onClick={addtext} className='px-4 py-2 rounded-full bg-orange-600 text-center'>Add</button>
         </div>
         {todo.map((item,index)=>{
